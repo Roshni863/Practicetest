4ab362c36d1c4f1a07cd786d1bc2d5a422b26a0e
Roshni Nikale <Roshni Nikale@LAPTOP-6O94TVHM>
2024-10-02 17:09:13 +0200

    By Roshni

---
+++ TestingPractice/src/Test1/Demo.java
@@ -0,0 +1,14 @@
+package Test1;
+
+public class Demo {
+
+	public void m1()
+	{
+		System.out.println("hello roshni");
+	}
+	public static void main(String[] args) {
+		
+		Demo d=new Demo();
+				d.m1();
+}
+}
